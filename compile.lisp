a7469e5d94a880b415cceae00e838929ce5c2162
Christoph Breitkopf <chbreitkopf@gmail.com>
2025-01-20 09:53:06 +0100

    Fix asdf not loading dependencies

--- compile.lisp
+++ compile.lisp
@@ -1,5 +1,6 @@
 #+CCL (require 'asdf)
 (pushnew (uiop/os:getcwd) quicklisp:*local-project-directories* :test #'equalp)
+(ql:quickload '("alexandria" "clingon"))
 #-ECL (asdf:make "m3f/executable")
 #+ECL (progn (ql:quickload '("m3f" "m3f/executable"))
 	     (asdf:make-build :m3f/executable :type :program :move-here #P"./src"
